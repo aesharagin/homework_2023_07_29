a0d1865ac609929aa9c4e491438e09f067d5d3c4
Alexey <sharagin_1993@mail.ru>
2023-07-23 22:27:28 +0300

    Задачи

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 1: Задайте значение N. Напишите программу, которая выведет все ЧЕТНЫЕ натуральные числа в промежутке от 1 до N. Выполнить с помощью рекурсии.
+N = 5, M = 9 -> "6, 8"
+N = 2, M = 10 -> "2, 4, 6, 8, 10"*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+      if (count > n)
+    {
+        return;
+    }
+    
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + "Четное");
+    }
+}
